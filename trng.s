f737a64e9ca8e4ea35c7caa526bb52eaed1dfd73
GuoHaoLan <coopticks@hotmail.com>
2023-01-19 14:59:29 +0800

    Init

--- trng.s
+++ trng.s
@@ -2,8 +2,7 @@
 // func rdrand(ptr *uint64)
 TEXT ·rdrand(SB),NOSPLIT,$0-8
     LEAQ ptr+0(FP), DX
-    // RDRAND %RAX
-    BYTE $0x0f; BYTE $0xc7; BYTE $0xf0
+    RDRAND AX
     MOVQ AX, DX
     RET
 
